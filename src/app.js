ba0c49e28afca3fc27dd101eba095e9c1ffbeebf
owaisnaeem-coding <0hussainnaeem786@gmail.com>
2025-11-26 15:27:49 +0500

    FAQs section completed

--- src/app.js
+++ src/app.js
@@ -22,4 +22,32 @@ sidebarLinks.forEach(link => {
     link.addEventListener('click', closeSidebarFunc);
 });
 
+
+const faqItems = document.querySelectorAll("#faq-section .faq-item");
+
+faqItems.forEach(item => {
+    const header = item.querySelector(".faq-header");
+    const content = item.querySelector(".faq-content");
+    const arrow = item.querySelector(".arrow i");
+
+    header.addEventListener("click", () => {
+        const isHidden = content.classList.contains("hidden");
+
+        // Toggle current
+        content.classList.toggle("hidden");
+        arrow.classList.toggle("fa-plus")
+        arrow.classList.toggle("fa-minus")
+
+        // Close other items
+        faqItems.forEach(otherItem => {
+            if (otherItem !== item) {
+                const otherContent = otherItem.querySelector(".faq-content");
+                const otherArrow = otherItem.querySelector(".arrow i");
+                otherContent.classList.add("hidden");
+                otherArrow.classList.remove("fa-minus")
+                otherArrow.classList.add("fa-plus")
+            }
+        });
+    });
+});
         
